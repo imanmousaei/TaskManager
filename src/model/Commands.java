1331f62bcff6f27e8f5a040f85aaf98fcd98609d
Iman Mousaei <imanmousaei1379@gmail.com>
2020-04-13 17:27:00 +0430

    add -> create

--- src/model/Commands.java
+++ src/model/Commands.java
@@ -2,7 +2,7 @@ package model;
 
 public class Commands {
     public static String REMOVE = "rm";
-    public static String ADD = "add";
+    public static String CREATE = "cr";
     public static String EDIT = "edit";
     public static String NormalTask = "n";
     public static String TimedTask = "t";
