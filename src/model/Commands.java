fae61b1e6943609a9f24fb4038258853c779d050
Iman Mousaei <imanmousaei1379@gmail.com>
2020-04-13 17:31:10 +0430

    edit -> ed

--- src/model/Commands.java
+++ src/model/Commands.java
@@ -3,9 +3,9 @@ package model;
 public class Commands {
     public static String REMOVE = "rm";
     public static String CREATE = "cr";
-    public static String EDIT = "edit";
+    public static String EDIT = "ed";
     public static String NormalTask = "n";
     public static String TimedTask = "t";
-    public static String CheckList = "t";
+    public static String CheckList = "c";
 
 }
