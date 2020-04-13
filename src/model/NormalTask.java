d8771f3f0115c4ecf4f9f03bb674420428e0edc3
Iman Mousaei <imanmousaei1379@gmail.com>
2020-04-13 16:02:00 +0430

    added NormalTask

---
+++ src/model/NormalTask.java
@@ -0,0 +1,50 @@
+package model;
+
+public class NormalTask {
+    protected String title;
+    protected String description;
+    protected TaskStatus status;
+
+    public NormalTask() {
+    }
+
+    public NormalTask(String title, String description, TaskStatus status) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "NormalTask{" +
+                "title='" + title + " : " +
+                ", description='" + description + " , " +
+                ", status=" + status +
+                '}';
+    }
+
+}
---
+++ src/model/TaskStatus.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum TaskStatus {
+    completed, incomplete, ignored;
+}
