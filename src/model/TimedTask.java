07a5eba64d59166d0ca53ee97e50ad1aabf40c34
Iman Mousaei <imanmousaei1379@gmail.com>
2020-04-13 17:01:49 +0430

    dueTime changed ti deadline

--- src/model/TimedTask.java
+++ src/model/TimedTask.java
@@ -1,5 +1,21 @@
 package model;
 
 public class TimedTask extends NormalTask {
-    Date dueTime = new Date();
+    protected Date deadline = new Date();
+
+    public TimedTask() {
+    }
+
+    public TimedTask(String title, String description, Date deadline) {
+        super(title, description);
+        this.deadline = deadline;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
 }
