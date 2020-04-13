2ea3b2f0aa62860eaed230043c85a23dcd175261
Iman Mousaei <imanmousaei1379@gmail.com>
2020-04-13 17:26:21 +0430

    create commands added

--- src/model/Commands.java
+++ src/model/Commands.java
@@ -4,4 +4,8 @@ public class Commands {
     public static String REMOVE = "rm";
     public static String ADD = "add";
     public static String EDIT = "edit";
+    public static String NormalTask = "n";
+    public static String TimedTask = "t";
+    public static String CheckList = "t";
+
 }
