4b928d43a5f1f136d9d1ae6bddfc5c8921c25ae9
Iman Mousaei <imanmousaei1379@gmail.com>
2020-04-16 21:36:32 +0430

    add & get checklist items

--- src/model/CheckList.java
+++ src/model/CheckList.java
@@ -2,7 +2,15 @@ package model;
 
 import java.util.ArrayList;
 
-public class CheckList extends Task {
-    ArrayList<TimedTask> list;
-    // TODO
+public class CheckList extends NormalTask {
+    private ArrayList<TimedTask> list;
+
+    public void addItem(TimedTask task){
+        list.add(task);
+    }
+
+    public TimedTask readItem(int index){
+        return list.get(index);
+    }
+
 }
